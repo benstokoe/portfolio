30c53ccf8505179fd57bf564a535d4323d6d3b6e
Ben Stokoe <benstokoe1@googlemail.com>
2023-03-24 10:08:14 +0000

    update tech stats

--- slices/TechStats/TechStats.tsx
+++ slices/TechStats/TechStats.tsx
@@ -11,17 +11,17 @@ const TechStats = ({ slice }: SliceComponentProps<TechStatsSlice>) => (
       <div className="stats shadow w-full stats-vertical laptop:stats-horizontal bg-base-300">
         <div className="stat place-items-center">
           <p className="stat-title">React</p>
-          <p className="stat-value">10 years</p>
+          <p className="stat-value">9 years</p>
         </div>
 
         <div className="stat place-items-center">
           <p className="stat-title">Next.js</p>
-          <p className="stat-value">6 years</p>
+          <p className="stat-value">4 years</p>
         </div>
 
         <div className="stat place-items-center">
           <p className="stat-title">GraphQL</p>
-          <p className="stat-value">6 years</p>
+          <p className="stat-value">4 years</p>
         </div>
 
         <div className="stat place-items-center">
